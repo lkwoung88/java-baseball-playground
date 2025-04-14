929497cecd635cf6f4ac29f27acc3cd2208efcf2
lkwoung88 <lkwoung88@gmail.com>
2025-04-14 23:21:14 +0900

    test/ 미스 경우 테스트 추가

--- src/test/java/ball/BallTest.java
+++ src/test/java/ball/BallTest.java
@@ -3,41 +3,64 @@ package ball;
 import game.Ball;
 import game.Umpire;
 import game.status.BallStatus;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import static game.status.BallStatus.*;
+import static game.status.BallStatus.MISS;
 import static org.assertj.core.api.Assertions.assertThat;
 
 
 class BallTest {
 
-    @DisplayName("같은 수, 같은 자리수이면 스트라이크 처리한다.")
+    private static Umpire umpire;
+
+
+    @BeforeAll
+    static void setup() {
+        umpire = new Umpire();
+    }
+
+    @DisplayName("같은 수, 같은 자리수이면 '스트라이크' 처리한다.")
     @Test
-    void isStrike(){
+    void isStrike() {
         // given
         Ball targetBall = Ball.of(1, 2);
         Ball guessBall = Ball.of(1, 2);
-        Umpire umpire = new Umpire();
 
         // when
         BallStatus resultStatus = umpire.judge(targetBall, guessBall);
 
         // then
-        assertThat(resultStatus).isEqualTo(BallStatus.STRIKE);
+        assertThat(resultStatus).isEqualTo(STRIKE);
     }
 
-    @DisplayName("같은 수, 다른 자리수이면 볼 처리한다.")
+    @DisplayName("같은 수, 다른 자리수이면 '볼' 처리한다.")
     @Test
-    void isBall(){
+    void isBall() {
         // given
         Ball targetBall = Ball.of(1, 2);
         Ball guessBall = Ball.of(1, 3);
-        Umpire umpire = new Umpire();
 
         // when
         BallStatus resultStatus = umpire.judge(targetBall, guessBall);
 
         // then
-        assertThat(resultStatus).isEqualTo(BallStatus.BALL);
+        assertThat(resultStatus).isEqualTo(BALL);
+    }
+
+    @DisplayName("같은 수가 아니면 '미스' 처리한다.")
+    @Test
+    void isMiss() {
+        // given
+        Ball targetBall = Ball.of(1, 2);
+        Ball guessBall = Ball.of(2, 3);
+
+        // when
+        BallStatus resultStatus = umpire.judge(targetBall, guessBall);
+
+        // then
+        assertThat(resultStatus).isEqualTo(MISS);
     }
 }
